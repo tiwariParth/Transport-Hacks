e13f7110380081e42ed9799fe154423caa9d8bfa
tiwariParth <243mjparth@gmail.com>
2023-09-23 11:03:25 +0530

    feat: :fire: First commit
    In this commit the basic file structuring is done.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Header from './components/Header'
+const App = () => {
+  return (
+    <Router>
+      <Header/>
+      <Routes >
+        
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HashLink } from 'react-router-hash-link'
+
+const Header = () => {
+  return (
+    <div className='flex m-[2px]'>
+        
+    </div>
+  )
+}
+
+export default Header
